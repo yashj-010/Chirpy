487a76935d37ffb46e52b6f8668656987d67c3e5
Yash Jain <yj99901@gmail.com>
2026-07-20 09:01:39 +0000

    Serve static logo image

--- main.go
+++ main.go
@@ -6,16 +6,11 @@ import (
 )
 
 func main() {
-	// Create a new ServeMux
 	mux := http.NewServeMux()
 
-	// Create a file server for the current directory
 	fileServer := http.FileServer(http.Dir("."))
-
-	// Serve files from the root path
 	mux.Handle("/", fileServer)
 
-	// Create the HTTP server
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
@@ -23,7 +18,6 @@ func main() {
 
 	log.Println("Server running at http://localhost:8080")
 
-	// Start the server
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
