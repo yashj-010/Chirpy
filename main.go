de8835f5f167a4ea5b7b7fca1ea39b7a16b4d48c
Yash Jain <yj99901@gmail.com>
2026-07-20 09:03:42 +0000

    Add health check endpoint and update file server path

--- main.go
+++ main.go
@@ -1,24 +1,29 @@
 package main
 
 import (
-	"log"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
+	// Health check endpoint
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(http.StatusText(http.StatusOK)))
+	})
+
+	// File server
 	fileServer := http.FileServer(http.Dir("."))
-	mux.Handle("/", fileServer)
+
+	// Serve files under /app/
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
 
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
-	log.Println("Server running at http://localhost:8080")
-
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatal(err)
-	}
+	server.ListenAndServe()
 }
